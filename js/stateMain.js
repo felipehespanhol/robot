fe1c757e7088af3d9f15bb5045741da2a9985ff2
Felipe Hespanhol <felipeghespanhol@gmail.com>
2017-04-01 00:12:03 -0300

    world bounds collision

--- js/stateMain.js
+++ js/stateMain.js
@@ -12,6 +12,8 @@ var StateMain = {
     // good guys, explosions
     game.physics.startSystem(Phaser.Physics.ARCADE);
 
+    this.robotSize = 0.5;
+
     this.map = game.add.tilemap("map");
     this.map.addTilesetImage("tiles");
 
@@ -24,12 +26,15 @@ var StateMain = {
     this.robot.animations.add('walk', [10,11,12,13,14,15,16,17], 12, true);
     this.robot.animations.add('jump', [18,19,20,21,22,23,24,25], 12, true);
     this.robot.anchor.set(0.5, 0.5);
+    this.robot.scale.x = this.robotSize;
+    this.robot.scale.y = this.robotSize;
+
     game.physics.enable(this.robot, Phaser.Physics.ARCADE);
 
     this.robot.animations.play('idle');
     this.robot.body.gravity.y = 100;
     this.robot.body.bounce.set(0.25);
-    this.robot.body.collideWorldBound = true;
+    this.robot.body.collideWorldBounds = true;
 
     game.camera.follow(this.robot);
     cursors = game.input.keyboard.createCursorKeys();
@@ -48,9 +53,9 @@ var StateMain = {
     }
 
     if (this.robot.body.velocity.x > 0) {
-      this.robot.scale.x = 1;
+      this.robot.scale.x = this.robotSize;
     } else {
-      this.robot.scale.x = -1;
+      this.robot.scale.x = -this.robotSize;
     }
 
     if (cursors.left.isDown) {
